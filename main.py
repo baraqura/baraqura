1ba890a12cc6acc3b636d1dd87abcce23e4d0e04
baraqura <baraqura@gmail.com>
2026-04-08 23:39:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import sys
+import os
+
+# পাইথনকে আপনার বর্তমান ফোল্ডার পাথ চিনিয়ে দেওয়া
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# এবার ভেতরের মেইন অ্যাপ ইমপোর্ট করা
 from app.core.main import app
