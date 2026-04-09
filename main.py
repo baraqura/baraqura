5a11c5b83715ffb07df8b3ad6007137d6f318d23
baraqura <baraqura@gmail.com>
2026-04-09 13:58:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-import os
+from app.core.main import app
 
-# পাইথনকে পাথ চিনিয়ে দেওয়া
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# ভেতরের অ্যাপ ইমপোর্ট করা
-try:
-    from app.core.main import app
-except ImportError as e:
-    print(f"Import Error: {e}")
-    raise e
+# Vercel কে সরাসরি অ্যাপটি চিনিয়ে দেওয়া
+app = app
