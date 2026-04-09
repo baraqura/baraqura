a58d20df53af03bcb73bec75a3ed0f05cfcf33a7
baraqura <baraqura@gmail.com>
2026-04-09 12:50:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
+# পাইথনকে পাথ চিনিয়ে দেওয়া
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# ভেতরের অ্যাপ ইমপোর্ট করা
 try:
-    # পথ চিনিয়ে দেওয়া
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     from app.core.main import app
-    print("V10 App loaded successfully!")
-except Exception as e:
-    print(f"ERROR LOCATED: {e}")
-    # এটি ভুল খুঁজে পেতে সাহায্য করবে
+except ImportError as e:
+    print(f"Import Error: {e}")
     raise e
